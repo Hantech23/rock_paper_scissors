1dff2dcb5a36761182c5408aaa6a113b5b9aa6ab
Woobin Han <woobin.han23@gmail.com>
2022-04-03 14:48:21 -0400

    Linked buttons with game function

--- main.js
+++ main.js
@@ -1,94 +1,75 @@
-/*
+//VARIABLES
+let rock = 'rock';
+let paper = 'paper';
+let scissors = 'scissors';
+
+//COMPUTER PLAY
+let array = [rock, paper, scissors];
+function computerPlay() {
+    return array[Math.floor(Math.random() * 3)];
+}
+
+//SCOREBOARD
 let playerScore = 0;
-let botScore = 0;
-let r = 'r';
-let p  = 'p';
-let s = 's';
-let array = [r, p, s];
-
-console.log('Game Start')
-scoreboard()
-
-
-while (true) {
-    //This is the rng for the bot's move
-    let botMove = array[Math.floor(Math.random() * 3)];
-    console.log(botMove)
-
-    //user input
-    let playerMove = prompt();
-    console.log(playerMove)
-
-
-    switch (playerMove) {
-        case r:
-            if (botMove == p) {
-                botScore++;
-            }
-            else if (botMove == s) {
-                playerScore++;
-            }
-            break;
-        case p:
-            if (botMove == r) {
-                playerScore++;
-            }
-            else if (botMove == s) {
-                botScore++;
-            }
-            break;
-        case s:
-            if (botMove == r) {
-                botScore++;
-            }
-            else if (botMove == p) {
-                playerScore++;
-            }
-            break;
-        default:
-            console.log('wtf just happened')
-            break;
-    }
-    scoreboard()
-    if(playerScore == 3) {
-        console.log('You won')
-        break;
-    }
-    if(botScore == 3) {
-        console.log('You lose')
-        break;
-    }
-}
-
+let computerScore = 0;
 function scoreboard(){
-    console.log("player: " + playerScore + " bot: " + botScore)
+    console.log("Player: " + playerScore + " Computer: " + computerScore);
 }
-*/
+
+//ROUND PLAY
+function roundPlay(playerSelection, computerSelection) {    
+    switch (playerSelection) {
+        case rock:
+            if (computerSelection == paper) {
+                computerScore++;
+            }
+            else if (computerSelection == scissors) {
+                playerScore++;
+            }
+            break;
+        case paper:
+            if (computerSelection == rock) {
+                playerScore++;
+            }
+            else if (computerSelection == scissors) {
+                computerScore++;
+            }
+            break;
+        case scissors:
+            if (computerSelection == rock) {
+                computerScore++;
+            }
+            else if (computerSelection == paper) {
+                playerScore++;
+            }
+            break;
+    }
+    scoreboard();  
+}
+console.log('test game:');
+roundPlay(rock, computerPlay());
+console.log('end test');
+
+//BUTTONS
 const body = document.querySelector('body');
 
 const rockBtn = document.createElement('button');
-rockBtn.classList.add('rock');
 rockBtn.textContent = 'Rock';
-rockBtn.addEventListener('click', playerMove);
+rockBtn.addEventListener('click', () => roundPlay(rock, computerPlay()));
 
 const paperBtn = document.createElement('button');
-paperBtn.classList.add('paper');
 paperBtn.textContent = 'Paper';
+paperBtn.addEventListener('click', () => roundPlay(paper, computerPlay()));
 
 const scissorsBtn = document.createElement('button');
-scissorsBtn.classList.add('scissors');
 scissorsBtn.textContent = 'Scissors';
+scissorsBtn.addEventListener('click', () => roundPlay(scissors, computerPlay()));
+
 
 body.append(rockBtn, paperBtn, scissorsBtn);
 
-
-function testfunction() {
-    console.log('testing');
-}
-
-//const rockBtn = document.querySelector('.rock');
-//rockBtn.addEventListener('click', testfunction);
-
-function playerMove(playerMove) {
-    console.log('you have selected' + playerMove);
-}
+/*
+const test = document.createElement('button');
+test.textContent = 'Test Button';
+test.addEventListener('click', )
+*/
